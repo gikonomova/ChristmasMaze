0a01bde9ac0ca582abb220392fa71badaf070067
gikonomova <g.ikonomova@gmail.com>
2015-12-15 22:15:07 +0200

    Implemented some of the item, inventory and character logic.

---
+++ ChristmasMaze.Data/Classes/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChristmasMaze.Common.Enums;
+using ChristmasMaze.Data.Interfaces;
+
+namespace ChristmasMaze.Data.Classes
+{
+    public class Inventory : IInventory
+    {
+        private List<Item> items;
+
+        public Inventory()
+        {
+                items = new List<Item>();
+        }
+        public List<Item> Items
+        {
+            get { return this.items; }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public double CalculateItemClassStats(ItemClass itemClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CollectItem(Item item)
+        {
+            this.Items.Add(item);
+        }
+    }
+}
---
+++ ChristmasMaze.Data/Interfaces/IItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChristmasMaze.Common.Enums;
+
+namespace ChristmasMaze.Data.Interfaces
+{
+    public interface IItem
+    {
+        string Name { get; set; }
+        ItemType Type { get; set; }
+        double AttackPoints { get; set; }
+        double DefensePoints { get; set; }
+        void CalculateItemTypeBonus();
+    }
+}
